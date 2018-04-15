f422a43af2fb90b66282fa9311913c9cdf0ca3d8
Nawin Tamang <37071638+tamangnawin@users.noreply.github.com>
2018-04-15 17:17:16 +0545

    li changed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,6 +11,9 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="#">Fashion0</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="#">Fashion</a>
       </li>
       <li class="nav-item">
